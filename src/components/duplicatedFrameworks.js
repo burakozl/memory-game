a086e98f30a245ea6cfbcfb4b1c7063537ea86b3
Burak Özel <burakk.oozel@gmail.com>
2022-06-29 18:18:09 +0300

    memory-game-redux

---
+++ src/components/duplicatedFrameworks.js
@@ -0,0 +1,27 @@
+export const frameworks = [
+    "angular2",
+    "vue",
+    "react",
+    "grunt",
+    "phantomjs",
+    "ember",
+    "babel",
+    "ionic",
+    "gulp",
+    "meteor",
+  ]; 
+  export const duplicatedFrameworks = [...frameworks, ...frameworks];
+  export const shuffle = (array) => {
+    let currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+  
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  };
